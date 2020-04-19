e437c7626c96e0f0747662b3f01982054e10ac3c
Nick G <nlgarwood@gmail.com>
2020-04-19 08:17:33 +0100

    Removed extra logging

--- templates/theme569/html/com_virtuemart/cart/select_shipment.php
+++ templates/theme569/html/com_virtuemart/cart/select_shipment.php
@@ -40,59 +40,57 @@ defined('_JEXEC') or die('Restricted access');
 		$buttonclass = 'vm-button';
 	}
 
-	// echo "<h".$headerLevel." class='cart-form_title'>".JText::_('COM_VIRTUEMART_CART_SELECT_SHIPMENT')."</h".$headerLevel.">"; ?>
+	// echo "<h".$headerLevel." class='cart-form_title'>".JText::_('COM_VIRTUEMART_CART_SELECT_SHIPMENT')."</h".$headerLevel.">";
+    ?>
 
     <?php
 
-    // H.U.G.E delivery only available initially Sat/Sun
-    $dayOfWeek = date("D");
-
-    // log dayOfWeek to file
-    file_put_contents('/home/hp3-linc7-nfs1-x/304/1810304/user/shipment.log', 'Day of week: '.$dayOfWeek . PHP_EOL, FILE_APPEND) ;
-    $whereString = '';
-    if ($dayOfWeek != 'Sat' && $dayOfWeek != 'Sun') {
-    $whereString = ' WHERE `virtuemart_shipmentmethod_id` <> 6';
-    }
+        // H.U.G.E delivery only available initially Sat/Sun
+        $dayOfWeek = date("D");
+        $whereString = '';
+        if ($dayOfWeek != 'Sat' && $dayOfWeek != 'Sun') {
+        $whereString = ' WHERE `virtuemart_shipmentmethod_id` <> 6';
+        }
 
     ?>
 		
 	<div class="options-list">
 		<?php
-	    if ($this->found_shipment_method  ) {
-            file_put_contents('/home/hp3-linc7-nfs1-x/304/1810304/user/shipment.log', PHP_EOL . 'Shipment Rates: '. print_r($this->shipments_shipment_rates, true) . PHP_EOL, FILE_APPEND) ;
-            ?>
-		<ul>
-		<?php
-            // H.U.G.E delivery only available initially Sat/Sun
-            $dayOfWeek = date("D");
-            $hugeDeliveryAvailable = false;
-            if ($dayOfWeek == 'Sat' || $dayOfWeek == 'Sun') {
-                $hugeDeliveryAvailable = true;
-            }
+	    if ($this->found_shipment_method  ) { ?>
 
-		    // if only one Shipment , should be checked by default
-		    foreach ($this->shipments_shipment_rates[0] as $idx => $shipment_shipment_rate) {
-                //file_put_contents('/home/hp3-linc7-nfs1-x/304/1810304/user/shipment.log', PHP_EOL . 'Shipment Rates Index: '. print_r($idx, true) . PHP_EOL, FILE_APPEND) ;
-                //file_put_contents('/home/hp3-linc7-nfs1-x/304/1810304/user/shipment.log', PHP_EOL . 'Shipment Shipment Rates: '. print_r($shipment_shipment_rate, true) . PHP_EOL, FILE_APPEND) ;
+            <ul>
 
-                if ($hugeDeliveryAvailable == false && $idx == 0) {
-                    $shipment_shipment_rate = str_replace('input','input disabled', $shipment_shipment_rate) ;
-                    $shipment_shipment_rate = str_replace('input','input disabled', $shipment_shipment_rate) ;
-		           // continue; // First shipping method is H.U.G.E Delivery
+                <?php
+                // H.U.G.E delivery only available initially Sat/Sun
+                $dayOfWeek = date("D");
+                $hugeDeliveryAvailable = false;
+                if ($dayOfWeek == 'Sat' || $dayOfWeek == 'Sun') {
+                    $hugeDeliveryAvailable = true;
                 }
-                echo '<li>' . $shipment_shipment_rate . '</li>';
-                /*
-				if (is_array($shipment_shipment_rates)) {
-				    foreach ($shipment_shipment_rates as $shipment_shipment_rate) {
-						echo '<li>' . $shipment_shipment_rate . '</li>';
-				    }
-				}
-                */
-		    } ?>
-		</ul>
+
+                // if only one Shipment , should be checked by default
+                foreach ($this->shipments_shipment_rates[0] as $idx => $shipment_shipment_rate) {
+                    //file_put_contents('/home/hp3-linc7-nfs1-x/304/1810304/user/shipment.log', PHP_EOL . 'Shipment Rates Index: '. print_r($idx, true) . PHP_EOL, FILE_APPEND) ;
+                    //file_put_contents('/home/hp3-linc7-nfs1-x/304/1810304/user/shipment.log', PHP_EOL . 'Shipment Shipment Rates: '. print_r($shipment_shipment_rate, true) . PHP_EOL, FILE_APPEND) ;
+
+                    if ($hugeDeliveryAvailable == false && $idx == 0) {
+                        $shipment_shipment_rate = str_replace('input','input disabled', $shipment_shipment_rate) ;
+                        $shipment_shipment_rate = str_replace('input','input disabled', $shipment_shipment_rate) ;
+                       // continue; // First shipping method is H.U.G.E Delivery
+                    }
+                    echo '<li>' . $shipment_shipment_rate . '</li>';
+                    /*
+                    if (is_array($shipment_shipment_rates)) {
+                        foreach ($shipment_shipment_rates as $shipment_shipment_rate) {
+                            echo '<li>' . $shipment_shipment_rate . '</li>';
+                        }
+                    }
+                    */
+                } ?>
+            </ul>
 		<?php 
 	    } else {
-			//echo "<h".$headerLevel.">".$this->shipment_not_found_text."</h".$headerLevel.">";
+			echo "<span>Please enter your delivery details</span>";
 	    } ?>
     </div>
 	<div class="control-button">
